462662df3d2d519f7d5f1193d858a78d176804db
Olu <theoafactor@gmail.com>
2022-01-31 13:25:23 +0100

    Added theme buttons

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 let fireBtn = document.querySelector("#fire");
 let skyBtn = document.querySelector("#sky");
 let body = document.body;
+let cardBody = document.querySelector(".card-body");
 
 //when you click
 fireBtn.onclick = function(){
+
+    refreshPage();
     
     body.style.backgroundColor = "yellow";
+
+    const img = document.createElement("img");
+
+    img.src="assets/tik-tok-logo.png";
+
+   
+
+    cardBody.appendChild(img);
+
+    
 
 
 }
@@ -13,8 +26,17 @@ fireBtn.onclick = function(){
 
 skyBtn.onclick = function(){
 
+    refreshPage();
+
     body.style.backgroundColor = "blue";
 
 }
+
+
+function refreshPage(){
+    cardBody.innerHTML = "";
+}
+
+
 
 
