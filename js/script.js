b8491fe46a477bfb4a767227ac36e0788dde3b9a
Olu <theoafactor@gmail.com>
2022-01-31 12:17:20 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let fireBtn = document.querySelector("#fire");
+let skyBtn = document.querySelector("#sky");
+let body = document.body;
+
+//when you click
+fireBtn.onclick = function(){
+    
+    body.style.backgroundColor = "yellow";
+
+
+}
+
+
+skyBtn.onclick = function(){
+
+    body.style.backgroundColor = "blue";
+
+}
+
+
